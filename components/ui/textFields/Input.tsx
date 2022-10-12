49d53c8b84d3053b979235e61aab98bd2aa179d8
avvakumovid <avvakumovid@gmail.com>
2022-10-12 12:08:16 +0300

    feat[tabs]: add Input

---
+++ components/ui/textFields/Input.tsx
@@ -0,0 +1,83 @@
+import { StyleSheet, TextInput, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Colors } from '../../../styles';
+import EyeIcon from '../../../assets/Icons/EyeIcon';
+
+interface InputProps {
+  placeholder: string;
+  lable: string;
+  isError?: boolean;
+  isSuccess?: boolean;
+}
+//TODO: Cursor Color
+const Input = ({
+  placeholder,
+  isError = false,
+  isSuccess = false,
+  lable,
+}: InputProps) => {
+  const [isActive, setActive] = useState(false);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.lable}>{lable}</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onFocus={() => {
+            setActive(true);
+          }}
+          onBlur={() => {
+            setActive(false);
+          }}
+          selectionColor={Colors.WHITE}
+          placeholder={isActive ? '' : placeholder}
+          placeholderTextColor={Colors.GREY}
+          style={[
+            styles.input,
+            {
+              color: isError
+                ? Colors.RED_U
+                : isSuccess
+                ? Colors.GREEN_U
+                : Colors.WHITE,
+            },
+          ]}
+        ></TextInput>
+        <EyeIcon
+          style={styles.icon}
+          color={isActive ? Colors.WHITE : Colors.GREY}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Input;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: 351,
+    height: 54,
+    borderRadius: 8,
+    backgroundColor: Colors.BLACK_1,
+    paddingHorizontal: 18,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    maxWidth: 295,
+  },
+  icon: {
+    marginLeft: 10,
+  },
+  lable: {
+    color: Colors.WHITE,
+    alignSelf: 'flex-start',
+    marginBottom: 8,
+  },
+});
